c48a2d084e410e3affbb0a1d6a3fd46451bf9763
mael098 <juanvillegasmorales@outlook.com>
2026-03-23 02:43:35 -0600

    commit

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
-def main():
-    print("Hello from python!")
+import json
+import os
+from typing import Optional, TypeVar, Iterable, Callable, TypedDict, cast
+
+class User(TypedDict):
+    firstname: str
+    lastname: str
+    username: str
+    email: str
+    age: int
+    id: int
+
+T = TypeVar("T")
+
+def find(iter: Iterable[T], fn: Callable[[T], bool]) -> Optional[T]:
+    for item in iter:
+        if fn(item):
+            return item
+    return None
+    
+
+with open(os.path.join('db', 'users.json')) as f:
+    data = cast(list[User], json.load(f))
+    
+print('Users:')
 
 
-if __name__ == "__main__":
-    main()
+user20 = find(data, lambda x: x['id'] == 3)
+
+
+
+
+print(user20)
