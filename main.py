080f278aef3d760730c6b51649ce902b890ab405
mael098 <juanvillegasmorales@outlook.com>
2026-03-22 21:17:55 -0600

    create init proyect

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-# This is a sample Python script.
-
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def main():
+    print("Hello from python!")
 
 
-diccionario = {
-    "nombre": "Juan",
-    "edad": 30,
-    "ciudad": "Madrid"
-}
-
-print(diccionario["nombre"])
-print(diccionario["edad"])
-print(diccionario["ciudad"])
+if __name__ == "__main__":
+    main()
