432ef0ade555b9ee9a148359385a0e7a98dc28f7
Elí Maciel <tecno_eli@hotmail.com>
2026-03-25 16:30:50 -0600

    fix types

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class Pipe(Generic[T]):
         self.process = []
         self.items = items
     
-    def map_if(self, cn: Callable[[T], bool], tr: Union[Callable[[T], T],Callable[[T, Pipe[T]], T]]):
+    def map_if(self, cn: Callable[[T], bool], tr: Union[Callable[[T], T], Callable[[T, "Pipe[T]"], T]]):
         for i in range(len(self.items)):
             if cn(self.items[i]):
                 try:
@@ -30,7 +30,7 @@ class Pipe(Generic[T]):
                 
         return self
     
-    def map(self, fn: Union[Callable[[T], T],Callable[[T, Pipe[T]], T]]):
+    def map(self, fn: Union[Callable[[T], T], Callable[[T, "Pipe[T]"], T]]):
         for i in range(len(self.items)):
             try:
                 self.items[i] = fn(self.items[i], self) # pyright: ignore[reportCallIssue]
@@ -38,7 +38,7 @@ class Pipe(Generic[T]):
                 self.items[i] = fn(self.items[i]) # pyright: ignore[reportCallIssue]
         return self
     
-    def filter(self, fn: Union[Callable[[T], bool], Callable[[T, Pipe[T]], bool]]):
+    def filter(self, fn: Union[Callable[[T], bool], Callable[[T, "Pipe[T]"], bool]]):
         nitems: list[T] = []
         
         for i in range(len(self.items)):
@@ -54,7 +54,7 @@ class Pipe(Generic[T]):
     def to_list(self):
         return self.items
     
-    def find(self, fn: Union[Callable[[T], bool], Callable[[T, Pipe[T]], bool]]) -> Optional[T]:
+    def find(self, fn: Union[Callable[[T], bool], Callable[[T, "Pipe[T]"], bool]]) -> Optional[T]:
         for item in self.items:
             try:
                 if fn(item, self): # pyright: ignore[reportCallIssue]
