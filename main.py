27e33425d67173823c4ce6dd5175cf7294148af9
Elí Maciel <tecno_eli@hotmail.com>
2026-03-25 15:00:19 -0600

    Pipe class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+from typing import Optional, TypeVar, Callable, TypedDict, cast, Generic
 import json
 import os
-from typing import Optional, TypeVar, Iterable, Callable, TypedDict, cast
 
 class User(TypedDict):
     firstname: str
@@ -12,17 +12,33 @@ class User(TypedDict):
 
 T = TypeVar("T")
 
-def find(iter: Iterable[T], fn: Callable[[T], bool]) -> Optional[T]:
-    for item in iter:
-        if fn(item):
-            return item
-    return None
-    
-
 with open(os.path.join('db', 'users.json')) as f:
     data = cast(list[User], json.load(f))
     
-user20 = find(data, lambda x: x['id'] == 3)
+class Pipe(Generic[T]):
+    def __init__(self, items: list[T]):
+        self.process = []
+        self.items = items
+    
+    def map_if(self, cn: Callable[[T], bool], tr: Callable[[T], T]):
+        for i in range(len(self.items)):
+            if cn(self.items[i]):
+                self.items[i] = tr(self.items[i])
+        return self
+    
+    def map(self, fn: Callable[[T], T]):
+        self.items = list(map(fn, self.items))
+        return self
+    
+    def filter(self, fn: Callable[[T], bool]):
+        self.items = list(filter(fn, self.items))
+        return self
 
-print(user20)
-
+    def to_list(self):
+        return list(self.items)
+    
+    def find(self, fn: Callable[[T], bool]) -> Optional[T]:
+        for item in self.items:
+            if fn(item):
+                return item
+        return None
