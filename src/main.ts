98afe58465ef1780a6e1357c778adbd62dc4ee92
Elí Maciel <tecno_eli@hotmail.com>
2026-03-25 13:59:11 -0600

    fix script

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { fa, Faker, faker } from "@faker-js/faker";
 import { User } from "#/classes/user.ts";
-import { writeFile } from "node:fs/promises";
+import { writeFile, mkdir } from "node:fs/promises";
 import { join } from "node:path";
 faker.seed(123);
 
@@ -10,4 +10,5 @@ for (let i = 0; i < 100; i++) {
   list.push(new User());
 }
 
+await mkdir(join("db"), { recursive: true })
 await writeFile(join("db", "users.json"), JSON.stringify(list, null, 2));
