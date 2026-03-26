99305f21bd6594e333b73ed7bb13263d2caa37db
Elí Maciel <tecno_eli@hotmail.com>
2026-03-26 01:10:45 -0600

    lambdas

--- src/classes/pipe.py
+++ src/classes/pipe.py
@@ -2,12 +2,13 @@ from typing import Optional, TypeVar, Callable, Generic, Union
 
 T = TypeVar("T")
 
-class Pipe(Generic[T]):
+U = TypeVar('U')
+
+class pipe(Generic[T]):
     def __init__(self, items: list[T]):
-        self.process = []
-        self.items = items
+        self.items = items.copy()
     
-    def map_if(self, cn: Callable[[T], bool], tr: Union[Callable[[T], T], Callable[[T, "Pipe[T]"], T]]):
+    def map_if(self, cn: Callable[[T], bool], tr: Union[Callable[[T], T], Callable[[T, "pipe[T]"], T]]):
         for i in range(len(self.items)):
             if cn(self.items[i]):
                 try:
@@ -17,15 +18,17 @@ class Pipe(Generic[T]):
                 
         return self
     
-    def map(self, fn: Union[Callable[[T], T], Callable[[T, "Pipe[T]"], T]]):
-        for i in range(len(self.items)):
+    def map(self, fn: Union[Callable[[T], U], Callable[[T, "pipe[T]"], U]]) -> "pipe[U]":
+        new_items: list[U] = []
+        for item in self.items:
             try:
-                self.items[i] = fn(self.items[i], self) # pyright: ignore[reportCallIssue]
+                new_items.append(fn(item, self)) # type: ignore
             except TypeError:
-                self.items[i] = fn(self.items[i]) # pyright: ignore[reportCallIssue]
-        return self
+                new_items.append(fn(item)) # type: ignore
+                
+        return pipe(new_items)
     
-    def filter(self, fn: Union[Callable[[T], bool], Callable[[T, "Pipe[T]"], bool]]):
+    def filter(self, fn: Union[Callable[[T], bool], Callable[[T, "pipe[T]"], bool]]):
         nitems: list[T] = []
         
         for i in range(len(self.items)):
@@ -36,12 +39,12 @@ class Pipe(Generic[T]):
                 if fn(self.items[i]): # pyright: ignore[reportCallIssue]
                     nitems.append(self.items[i])
                 
-        return Pipe(nitems)
+        return pipe(nitems)
 
     def to_list(self):
         return self.items
     
-    def find(self, fn: Union[Callable[[T], bool], Callable[[T, "Pipe[T]"], bool]]) -> Optional[T]:
+    def find(self, fn: Union[Callable[[T], bool], Callable[[T, "pipe[T]"], bool]]) -> Optional[T]:
         for item in self.items:
             try:
                 if fn(item, self): # pyright: ignore[reportCallIssue]
--- src/main.py
+++ src/main.py
@@ -1,18 +1,62 @@
 from typing import Optional, TypedDict, cast
+from rich import print
 import json
 import os
-from classes.pipe import Pipe
+from classes.pipe import pipe
 
 class User(TypedDict):
     firstname: str
     lastname: str
-    username: Optional[str]
-    email: str
+    username: str
+    email: Optional[str]
     age: int
     id: int
 
 with open(os.path.join('db', 'users.json')) as f:
     data = cast(list[User], json.load(f))
 
-Pipe([])
+# ----------------------------
+# ----------- FIND -----------
+# ----------------------------
+user30 = pipe(data).find(lambda u: u['id'] == 30)
+
+print('User 30')
+print(json.dumps(user30, indent=4))
+
+# ----------------------------
+# ------- FILTER y MAP -------
+# ----------------------------
+"""
+Se toman los usuarios sin correo como anonimos asi que se filtran
+y por ultimo se mapean para mostrar unicamente su nombre de usuario
+"""
+anonimous = pipe(data)\
+    .filter(lambda u: not u['email'])\
+    .map(lambda u: u['username'])\
+    .to_list()
+
+print()
+print('Usuarios anonimos:')
+print(anonimous)
+
+# ----------------------------
+# --- FILTER, MAP Y MAP_IF ---
+# ----------------------------
+normalized = pipe(data)\
+    .filter(lambda u: u['age'] >= 18)\
+    .map_if(
+        lambda u: not u['email'], 
+        lambda u: { 
+            **u, 
+            'email': f'{u['firstname'].lower()}_{u['lastname'].lower()}@enterprice.com' 
+        }
+    )\
+    .map(lambda u: { 
+        **u, 
+        'username': u['username'].lower()
+    })\
+    .to_list()
     
+print()
+print('Normalizados')
+print(normalized)
